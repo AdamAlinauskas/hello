3c73b481e925a48d0c346fdaef07f5270b75e746
CodingWith-Adam <adam@example.com>
2020-04-24 18:47:29 -0600

    Updates

---
+++ precache-manifest.060d866a31072ed0bfa794c360bc8eb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2644425accd7bd99913aa5043f79b18",
+    "url": "/AdamAlinauskas/hello/index.html"
+  },
+  {
+    "revision": "56be3c62e77fd6aff8ce",
+    "url": "/AdamAlinauskas/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b3ac96927ca966194456",
+    "url": "/AdamAlinauskas/hello/static/js/2.5260cd7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AdamAlinauskas/hello/static/js/2.5260cd7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56be3c62e77fd6aff8ce",
+    "url": "/AdamAlinauskas/hello/static/js/main.faa92f27.chunk.js"
+  },
+  {
+    "revision": "566d265bf3801ca387ba",
+    "url": "/AdamAlinauskas/hello/static/js/runtime-main.9e62a2d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AdamAlinauskas/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
